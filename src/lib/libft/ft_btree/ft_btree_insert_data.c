ea276fea8b1bf437b01db4e16b2dc2156aa3d499
Tetsuro Ando <tetsuro997@gmail.com>
2025-05-23 19:53:34 +0900

    Add libft files as regular files instead of submodule

---
+++ src/lib/libft/ft_btree/ft_btree_insert_data.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree_insert_data.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 07:32:09 by teando            #+#    #+#             */
+/*   Updated: 2025/05/22 23:37:40 by teando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	ft_btree_insert_data(t_btree **root, void *item, int (*cmpf)(void *,
+			void *))
+{
+	if (!root)
+		return ;
+	if (*root == 0)
+	{
+		*root = ft_btree_create_node(item);
+		return ;
+	}
+	if (cmpf(item, (*root)->item) < 0)
+		ft_btree_insert_data(&((*root)->left), item, cmpf);
+	else
+		ft_btree_insert_data(&((*root)->right), item, cmpf);
+}
