d90ffeca137e35045c3d28ed22a6050efe0c4741
Tomoki Sato <60466946+tomsato42@users.noreply.github.com>
2025-05-23 22:27:25 +0900

    Merge pull request #20 from TetsuroAndo/hotfix_strtod

--- src/lib/xlib/ft_strtod.c
+++ src/lib/xlib/ft_strtod.c
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 18:11:02 by tomsato           #+#    #+#             */
-/*   Updated: 2025/05/14 16:01:09 by teando           ###   ########.fr       */
+/*   Updated: 2025/05/23 23:01:07 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,7 @@
 #define DOT '.'
 #define BASE 10
 
-static double	my_pow(double x, double y)
-{
-	double	result;
-	double	i;
-
-	result = x;
-	i = 1;
-	while (i < y)
-	{
-		result *= x;
-		i++;
-	}
-	return (result);
-}
-
+/* 小数点以降の桁数をカウントする関数 */
 static size_t	count_decimal_digits(const char *decimal_start)
 {
 	size_t	digits;
@@ -46,21 +32,24 @@ double	ft_strtod(const char *nptr, char **endptr)
 	long	b_dot;
 	size_t	digits;
 	double	frac;
+	int		sign;
 
+	sign = 1;
+	if (*nptr == '-')
+		sign = -1;
 	b_dot = ft_strtol(nptr, endptr, BASE);
 	nptr = *endptr;
 	if (*nptr == DOT)
 	{
-		++nptr;
-		digits = count_decimal_digits(nptr);
+		digits = count_decimal_digits(++nptr);
 		*endptr = (char *)nptr + digits;
 		if (digits > 0)
 		{
 			a_dot = ft_strtol(nptr, endptr, BASE);
-			frac = (double)a_dot / my_pow(10.0, (double)digits);
+			frac = (double)a_dot / pow(10.0, (double)digits);
 			if (b_dot < 0)
-				return ((double)b_dot - frac);
-			return ((double)b_dot + frac);
+				return (((double)b_dot - frac));
+			return (sign * ((double)b_dot + frac));
 		}
 	}
 	return ((double)b_dot);
